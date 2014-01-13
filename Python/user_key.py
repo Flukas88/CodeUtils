d76c691349477b0dc12e1d72692535463cf589da
Luca Francesca <luca.francesca01@gmail.com>
2014-01-13 12:06:15 +0100

    More randomness
    Added more randomness

--- Python/user_key.py
+++ Python/user_key.py
@@ -10,13 +10,14 @@ import string
 
 def keyGen(uname, pwd):
     key = random.choice(string.lowercase)
-    key += uname[1:5]
+    key += uname[1:4]
     key += pwd[2:4] + random.choice(string.uppercase)
     key += random.choice(string.lowercase)
     key += str(random.randint(2,9)) 
-    key += str(random.randrange(1,10+1))
+    key += str(random.randrange(1,10) * 3)
     key += random.choice(string.uppercase)
     key += random.choice(string.lowercase)
+    key += str(random.randint(random.randint(1,50) ,random.randint(50,70))) 
     return key;
 
 
