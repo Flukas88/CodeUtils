dde1b82c26707db36f21e92bdb350f53af4eeff3
Luca Francesca <luca.francesca01@gmail.com>
2014-01-21 14:12:55 +0100

    Added Python Mysql->JSON
    Added a script for translating mysql to json

---
+++ Python/MysqlToJson.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import MySQLdb
+import json
+import cgi, cgitb
+
+host  = "xxxx"
+uname = "xxxx"
+pwd   = "xxxx"
+dbname = "xxxx"
+db    =  MySQLdb.connect(host,uname,pwd,dbname)
+cur   =  db.cursor()
+form  =  cgi.FieldStorage()
+    
+cmd =  "select * from Notes where text like '%"
+cmd +=  form.getvalue('text')
+cmd += "%';"
+
+content = {}
+
+try:
+   cur.execute(cmd)
+except:
+   content['status'] = 'failed'
+
+for row in cur.fetchall() :
+   content[row[0]] = row[1]
+
+print json.dumps(content, ensure_ascii=False)
+db.close()
