b9cf03956080cda58895bbdef8e092a11777a79c
Luca Francesca <luca.francesca01@gmail.com>
2014-07-02 14:24:46 +0200

    Added functions
    Added readableNumberLimiter, isIE, trim

--- Js/utils.js
+++ Js/utils.js
@@ -1,3 +1,19 @@
 function encodeHTML(s) {
     return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 }
+
+String.prototype.trim = function(){ this.replace( /^\s+|\s+$/g, '' ); }
+
+function isIE() {
+    return '\v' == 'v';
+}
+
+// Example of use readableNumberLimiter(12345678,",");  and I get "12,345,678"
+readableNumberLimiter = function (number,limiter)
+{
+  return number.toString().replace(
+     new RegExp(
+         "(^\\d{"+(number.toString().length%3||-1)+"})(?=\\d{3})"),
+         "$1"+limiter
+         ).replace(/(\d{3})(?=\d)/g,"$1"+limiter);
+}
