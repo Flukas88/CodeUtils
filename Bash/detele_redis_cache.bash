94535706b6572b813ef9cc94cf879516db02c5ec
Luca Francesca <luca.francesca@linux.com>
2015-03-20 19:00:34 +0100

    More modular
    Added some modularity

--- Bash/detele_redis_cache.bash
+++ Bash/detele_redis_cache.bash
@@ -2,19 +2,25 @@
 # delete keys from DBN cache on port PORT starting from CUR till the end in STEP steps on host HOST
 
 CUR=0
-DBN=2
-STEP=100
-PORT=6380
-HOST="localhost"
+PGNAME=`basename $0`
+STEP=10000
+PORT=$1
+DB=$2
+EXEC="/export/local/redis/bin/redis-cli"
+
+if [[ $# -ne 3 ]]; then
+    echo "usage: $PGNAME PORT DB_NUMBER"
+    exit 55
+fi
 
 while :
 do
-    /export/local/redis/bin/redis-cli -h "$HOST" -p "$PORT" -n "$DBN" scan "$CUR" COUNT "$STEP" > keybatch.txt
+    $EXEC -p "$PORT" -n "$DB" scan "$CUR" COUNT "$STEP" > keybatch.txt
     CUR=$(head -1 keybatch.txt)
     echo "CUR at $CUR"
     sed 's/^/del /' keybatch.txt > del.txt
-    /export/local/redis/bin/redis-cli -p "$PORT" -n "$DBN" < del.txt | wc  -l
-    sleep 30
+    $EXEC -p "$PORT" -n "$DBN" < del.txt | wc  -l
+    sleep 1
     if [[ $CUR -eq 0 ]]; then
         exit 0;
     fi
